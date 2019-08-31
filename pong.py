4a90c53c205f11f6f40d9df8f0f999a1918f5215
Tatty <tattyklass@gmail.com>
2019-08-31 16:56:46 -0700

    scoring

--- pong.py
+++ pong.py
@@ -7,6 +7,10 @@ wn.setup(width=800, height=600)
 # stops window from updating - speeds up game (?)
 wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -35,6 +39,15 @@ ball.goto(0, 0)
 #seperates balls movement into y and y, d is for delta
 ball.dx = 2
 ball.dy = -2
+
+#pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Function
 def paddle_a_up():
@@ -84,10 +97,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
     
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
 #Paddle and ball collisions
 
